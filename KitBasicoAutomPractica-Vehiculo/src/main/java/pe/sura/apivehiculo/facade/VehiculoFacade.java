42ef86d1a92cf73f67b5e3124ad95f45567a2abf
NP375128 <NP375128@P5GMD220.pe.intranet>
2018-05-15 17:18:51 -0500

    Se actualizan el paquete Facade

--- KitBasicoAutomPractica-Vehiculo/src/main/java/pe/sura/apivehiculo/facade/VehiculoFacade.java
+++ KitBasicoAutomPractica-Vehiculo/src/main/java/pe/sura/apivehiculo/facade/VehiculoFacade.java
@@ -1,13 +1,17 @@
 package pe.sura.apivehiculo.facade;
 
+
 import pe.sura.apivehiculo.entidades.Vehiculo;
 
 public class VehiculoFacade implements IVehiculoFacade {
 
 	@Override
 	public Vehiculo validar(Vehiculo vehiculo) {
-		// TODO Auto-generated method stub
-		return null;
+		
+			return null;
+		
 	}
-
+	
+	
+	
 }
