ec4418b8b460664265ded4a3bb32a875bee7bfa1
NP375128 <NP375128@P5GMD220.pe.intranet>
2018-05-15 16:46:48 -0500

    Se sube la clase VehiculosRepository

--- KitBasicoAutomPractica-Vehiculo/src/main/java/pe/sura/apivehiculo/datos/VehiculoRepository.java
+++ KitBasicoAutomPractica-Vehiculo/src/main/java/pe/sura/apivehiculo/datos/VehiculoRepository.java
@@ -3,7 +3,6 @@ package pe.sura.apivehiculo.datos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import pe.sura.apivehiculo.entidades.Vehiculo;
 
 @Repository
