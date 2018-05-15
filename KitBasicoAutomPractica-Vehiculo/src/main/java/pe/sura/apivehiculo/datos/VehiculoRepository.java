877eddc1e71a96123c4b6fb307e7a4b40bb4d71a
NP375128 <NP375128@P5GMD220.pe.intranet>
2018-05-15 16:44:59 -0500

    Se sube

---
+++ KitBasicoAutomPractica-Vehiculo/src/main/java/pe/sura/apivehiculo/datos/VehiculoRepository.java
@@ -0,0 +1,12 @@
+package pe.sura.apivehiculo.datos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import pe.sura.apivehiculo.entidades.Vehiculo;
+
+@Repository
+public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
+
+}
