b62b73bda01dcd1abb69a01d184220253bcd37d8
NP375128 <NP375128@P5GMD220.pe.intranet>
2018-05-16 15:56:04 -0500

    Se retira Json

--- KitBasicoAutomPractica-Vehiculo/src/main/java/pe/sura/apivehiculo/controller/VehiculoController.java
+++ KitBasicoAutomPractica-Vehiculo/src/main/java/pe/sura/apivehiculo/controller/VehiculoController.java
@@ -2,16 +2,13 @@ package pe.sura.apivehiculo.controller;
 
 
 
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import pe.sura.apivehiculo.entidades.VehiculoValidaResponse;
 import pe.sura.apivehiculo.facade.IVehiculoFacade;
 
