5b7f5ac13f80b229961496780867a7956d570f9d
NP375128 <NP375128@P5GMD220.pe.intranet>
2018-05-16 15:56:57 -0500

    Commit 2

--- KitBasicoAutomPractica-Vehiculo/src/test/java/pe/sura/apivehiculo/VehiculoFacadeTestConFake.java
+++ KitBasicoAutomPractica-Vehiculo/src/test/java/pe/sura/apivehiculo/VehiculoFacadeTestConFake.java
@@ -1,10 +1,9 @@
 package pe.sura.apivehiculo;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
